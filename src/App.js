a9a429943528161f7d22f988463bff39156d7a20
Yash Aggarwal <160060792+hecker2809@users.noreply.github.com>
2024-09-23 00:29:48 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ const App = () => {
       setErrorMessage('');
 
       // Call the API with parsed JSON input
-      const result = await axios.post('http://localhost:5000/bfhl', { data: parsedInput.data });
+      const result = await axios.post('https://vercel.com/yash-aggarwals-projects-42087377/bajaj-backend/FrKrP9LU3cWY8jLWfQkKCMVLutM8', { data: parsedInput.data });
       if (result.data.is_success) {
         setResponse(result.data);
         setFilteredResponse([]);  // Reset filtered response on new submit
